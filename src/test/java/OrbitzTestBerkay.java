1d5aecc518728a7bf64a929050a36790d20766b7
aigerim-aidilova <52512970+aigerim-aidilova@users.noreply.github.com>
2019-07-10 19:18:50 -0500

    test

--- src/test/java/OrbitzTestBerkay.java
+++ src/test/java/OrbitzTestBerkay.java
@@ -2,5 +2,6 @@ public class OrbitzTestBerkay {
     public static void main(String[] args) {
         System.out.println("karasho");
         System.out.println("hey");
+        System.out.println("git");
     }
 }
